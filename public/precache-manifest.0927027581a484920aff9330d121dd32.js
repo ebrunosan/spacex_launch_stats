719c76e456ec1eef8e625af357df82c2addb0df3
Bruno Santos <Bruno_Santos@worldvision.ca>
2019-06-26 23:00:03 -0400

    packages json updated

---
+++ public/precache-manifest.0927027581a484920aff9330d121dd32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222ea4b5e0cae0ef150cb73e51eca235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2909a024bb990d97fc64",
+    "url": "/static/css/main.5fd6d562.chunk.css"
+  },
+  {
+    "revision": "d08dcfb95d0fade9e8a2",
+    "url": "/static/js/2.d8e05170.chunk.js"
+  },
+  {
+    "revision": "2909a024bb990d97fc64",
+    "url": "/static/js/main.0daddb47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
